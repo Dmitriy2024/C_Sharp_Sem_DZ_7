141028add0f7ad8442713405e1e292df924ce770
Dmitriy <Dmitriy.Kh.78@yandex.ru>
2023-06-19 23:11:58 +0300

    Z2

--- Program.cs
+++ Program.cs
@@ -37,6 +37,7 @@ void PrintMatrix(int[,] array)
 }
 */
 
+/*
 //Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными 
 //числами.
 Console.Write("Введите m: ");
@@ -73,4 +74,52 @@ void PrintMatrix(double[,] array)
     Console.WriteLine();
    }
 }
+*/
 
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном
+// массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Console.Write("Введите индекс m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите индекс n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int row = 3;
+int column = 4;
+
+int[,] resArray = GetMatrix(row, column); 
+PrintMatrix(resArray);
+
+
+int[,] GetMatrix(int m, int n)
+{
+int[,] matrix = new int[row, column];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j <matrix.GetLength(1); j++)
+   {
+        matrix[i,j] = matrix[i,j] = new Random().Next(0, 100 + 1);
+    }
+}
+        return matrix;
+
+}
+void PrintMatrix(int[,] array)
+{
+ for(int i = 0; i < array.GetLength(0); i++)
+  
+   {
+      for(int j = 0; j < array.GetLength(1); j++)
+       {
+        Console.Write(array[i, j] + "\t");
+       }
+       Console.WriteLine();
+   }
+} 
+if(m > row || n > column)
+{
+  Console.Write("Эленент не существует");
+}
+else
+{
+   
+    Console.Write("Значение злемента = " + resArray[m-1, n-1] );
+}  
