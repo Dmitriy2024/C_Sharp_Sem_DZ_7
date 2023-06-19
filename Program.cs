81be3f64d7b0659ba83c5565a1ad19ec3b2a8981
Dmitriy <Dmitriy.Kh.78@yandex.ru>
2023-06-20 01:52:14 +0300

    Otpravka

--- Program.cs
+++ Program.cs
@@ -1,43 +1,4 @@
-﻿// Задать двумерный массив m*n, заполненый случайными числами.
-//m=3, n=4.
-//1 2 8 9
-//5 -2 33 -2
-//77 3 8 1
-/*
-Console.Write("Введите кол-во строк;");
-int rows = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите кол-во столюцов;");
-int colums = Convert.ToInt32(Console.ReadLine());
-
-//int[,] resMartrix = GetMatrix(rows, colums, 0, 10)
-
-int[,] GetMatrix(int m, int n, int min, int max)
-{
-int[,] matrix = new int[m,n];
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j <matrix.GetLength(1); j++)
-   {
-        matrix[i,j] = new Random().Next(min, max + 1);
-    }
-    }
-    return matrix;
-}
-void PrintMatrix(int[,] array)
-{
- for(int i = 0; i < array.GetLength(0); i++)
-   {
-    for(int j = 0; j < array.GetLength(0); j++)
-    {
-        Console.Write(array[i, j] + "\t");
-    }
-    Console.WriteLine();
-   }
-}
-*/
-
-/*
+﻿
 //Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными 
 //числами.
 Console.Write("Введите m: ");
@@ -74,8 +35,10 @@ void PrintMatrix(double[,] array)
     Console.WriteLine();
    }
 }
-*/
 
+
+
+/*
 //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном
 // массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 Console.Write("Введите индекс m: ");
@@ -123,3 +86,63 @@ else
    
     Console.Write("Значение злемента = " + resArray[m-1, n-1] );
 }  
+*/
+
+
+/*
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+//элементов в каждом столбце.
+int sumColumn = 0; 
+int sumC = 0;
+int row = 3;
+int column = 4;
+
+int[,] resArray = GetMatrix(row, column); 
+PrintMatrix(resArray);
+SumArray(resArray);
+
+
+
+
+int[,] GetMatrix(int m, int n)
+{
+int[,] matrix = new int[row, column];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j <matrix.GetLength(1); j++)
+   {
+        matrix[i,j] = matrix[i,j] = new Random().Next(0, 100 + 1);
+    }
+}
+        return matrix;
+
+}
+void PrintMatrix(int[,] array)
+{
+ for(int i = 0; i < array.GetLength(0); i++)
+  
+   {
+      for(int j = 0; j < array.GetLength(1); j++)
+       {
+        Console.Write(array[i, j] + "\t");
+       }
+       Console.WriteLine();
+   }
+}
+
+void SumArray(int[,] array)
+{
+    Console.WriteLine("Cреднее арифметическое элементов в каждом столбце.");
+ for(int i = 0; i < array.GetLength(1); i++)
+ {
+    sumColumn = 0;
+    for(int j = 0; j < array.GetLength(0); j++)
+   {
+    sumColumn = (sumColumn + array[j,i]);
+     
+   }   
+   
+Console.Write(" " + (sumColumn / 3) + "\t");
+  }
+}
+*/
